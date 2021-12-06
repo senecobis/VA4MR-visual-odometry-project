8847dd27c50cb21d0d86ef13978db6dee3fc7455
retoc71586 <99giaman99@gmail.com>
2021-12-06 23:06:05 +0100

    finito tutto per la prima volta, mancano i plotting

--- utilities/plotcameramov.m
+++ utilities/plotcameramov.m
@@ -1,18 +1,22 @@
 function t_n = plotcameramov(t,img, keypoints_img, t_prec,i)
 
-t_n = abs(t_prec) + abs(t);
-
+%t_n = abs(t_prec) + abs(t);
 figure(3)
+[x;y;z] = t*[0 0 0];
+subplot(1,2)
+plot()
+% 
+% plot(i, t_n(1), 'r.'); hold on
+% axis tight
+% subplot(2,3,2)
+% plot(i, t_n(2), 'r.'); hold on
+% subplot(2,3,3)
+% plot(i, t_n(3), 'r.'); hold on
 
-subplot(2,3,1)
-plot(i, t_n(1), '.'); hold on
-axis tight
-subplot(2,3,2)
-plot(i, t_n(2), '.'); hold on
-subplot(2,3,3)
-plot(i, t_n(3), '.'); hold on
+x_keypoints = keypoints_img(:,1);
+y_keypoints = keypoints_img(:,2);
 
-subplot(2,3,5)
+subplot(2,2)
 imshow(img,[]); hold on
-plot(keypoints_img(1,:), keypoints_img(2,:),'x');
+plot(x_keypoints,y_keypoints,'x');
 hold off

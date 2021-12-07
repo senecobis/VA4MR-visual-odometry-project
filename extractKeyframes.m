c9c333f398f9d9a6df8dc859a56459bdabb2ce74
retoc71586 <99giaman99@gmail.com>
2021-12-07 21:38:44 +0100

    Revert "risolto problema ransac. Ora devo capire come trovare nuovi keypoints"
    This reverts commit 84fc1a204178e39243f96a34173226c3800fcb07.

--- extractKeyframes.m
+++ extractKeyframes.m
@@ -6,23 +6,18 @@ function [S] = extractKeyframes(S0, S, T_C_W, img0, img1, K)
     T_C_W_hom = [T_C_W; 0 0 0 1];
     T_w_c = inv(T_C_W_hom);
     T_w_c = T_w_c(1:3,:);
-    
+
+
 % Create the point tracker
     tracker = vision.PointTracker('MaxBidirectionalError', 1, 'NumPyramidLevels', 5);
     
-% % Initialize the point tracker
-%     p0 = imagePoints0.Location;
-%     initialize(tracker, p0, img0);
-    
 % Initialize the point tracker
     initialize(tracker, S0.C', img0);
-    
 % Track the points
     [imagePoints1, validIdx] = step(tracker, img1);
     %matchedPoints0 = S0.C(:,validIdx);
     %elimino dallo stato i candidate keypoints non tracciati
     S.C = imagePoints1(validIdx, :);
-    S.C = S.C';
     S.F = S0.F(: ,validIdx);
     S.T = S0.T(: ,validIdx);
     %aggiungo allo stato i nuovi candidate keypoints presenti in S.p
@@ -34,30 +29,26 @@ function [S] = extractKeyframes(S0, S, T_C_W, img0, img1, K)
         end            
     end   
     
-% Update S.p and S.X with the keypoints that pass the nearing angle test
+% Triangulate a point cloud using the final transformation (R,T)
+% since we know the T_w_ci fr each component of S.C we can triangulate as a
+% stereo camera between the two poses to get X_c1 and transform them in W
+% later
     for candidate_idx = 1:height(S0.C)
-        p0 = S.F(:,candidate_idx);
+        p0 = S0.F(:,candidate_idx);
         p0 = [p0; 1];
         bearing_0 = K\p0;
-        p1 = S.C(:,candidate_idx);
+        p1 = S0.C(:,candidate_idx);
         p1 = [p1; 1];
         bearing_1 = K\p1;
         angolo = acos(dot(bearing_0,bearing_1)/(norm(bearing_0)*norm(bearing_1)));
         if angolo*pi/180 > 5
             M0 = K * reshape(S.T(:,candidate_idx),[3,4]);
-            M1 = K * T_w_c;
+            M1 = K * T_C_W;
             NewLandmarks = linearTriangulation(p0,p1,M0,M1);
-            S.X(:,end+1) = NewLandmarks(1:3,:);
-            S.p(:,end+1) = S.C(:,candidate_idx);
+            S.X = NewLandmarks(1:3,:);
         end
     end
 % forse l'update dei landmarks potrei farlo solo per i nuovi aggiunti ma ho
 % troppo sonno
-
-% Detect feature points
-    imagePoints0 = detectMinEigenFeatures(img0, 'MinQuality', 0.1);
-    candidate_keypoints = imagePoints0.Location';
-    S.C = [S.C, candidate_keypoints];
-
 end
 

70196a545579e38ae01b19871fda35a14202f1de
Roberto Pellerito <72160404+senecobis@users.noreply.github.com>
2021-12-09 10:33:49 +0100

    Delete Parameters.m

--- Parameters.m
+++
@@ -1,8 +0,0 @@
-global filtersize
-global quality
-global SURF_metric_treshold
-global stronghest_keypoints
-filtersize = 5;
-quality = 0.001;
-SURF_metric_treshold = 200;
-stronghest_keypoints = 200;
